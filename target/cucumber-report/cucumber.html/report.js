fe2e39967d974969032c032f6af2d8484cb89088
chaitanyamehta1 <mehtachaitanya1179@gmail.com>
2020-04-29 16:52:45 +0100

    barclays-bank-cucumber-project-master

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,225 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/uk/co/barclays/resources/featurefile/contactusandfindbranch.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Contact Us and Find a Branch Page navigate functionality",
+  "description": "As a\nUser I want to visit barclays bank website",
+  "id": "contact-us-and-find-a-branch-page-navigate-functionality",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 8434198700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Verify user should navigate to Contact Us page successfully",
+  "description": "",
+  "id": "contact-us-and-find-a-branch-page-navigate-functionality;verify-user-should-navigate-to-contact-us-page-successfully",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I click on Contact Us Button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "I should be in Contact us page successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iAmOnHomePage()"
+});
+formatter.result({
+  "duration": 128536179,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsAndFindABranchFeature.iClickOnContactUsButton()"
+});
+formatter.result({
+  "duration": 1794741754,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsAndFindABranchFeature.iShouldBeInContactUsPageSuccessfully()"
+});
+formatter.result({
+  "duration": 121406083,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 95171,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 6983536907,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Verify user should navigate to Find a Branch page successfully",
+  "description": "",
+  "id": "contact-us-and-find-a-branch-page-navigate-functionality;verify-user-should-navigate-to-find-a-branch-page-successfully",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 11,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "I click on Find a Branch button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "I should be in Find a Branch page successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iAmOnHomePage()"
+});
+formatter.result({
+  "duration": 31997,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsAndFindABranchFeature.iClickOnFindABranchButton()"
+});
+formatter.result({
+  "duration": 1421079750,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsAndFindABranchFeature.iShouldBeInFindABranchPageSuccessfully()"
+});
+formatter.result({
+  "duration": 332015696,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 37331,
+  "status": "passed"
+});
+formatter.uri("src/test/java/uk/co/barclays/resources/featurefile/loginandregister.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login and Register page navigate functionality",
+  "description": "As a user\nI want to visit barclays bank website",
+  "id": "login-and-register-page-navigate-functionality",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 5784929616,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Verify user should navigate to login page",
+  "description": "",
+  "id": "login-and-register-page-navigate-functionality;verify-user-should-navigate-to-login-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I click on Login button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "I should be in Login page successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iAmOnHomePage()"
+});
+formatter.result({
+  "duration": 30357,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iClickOnLoginButton()"
+});
+formatter.result({
+  "duration": 1443996481,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iShouldBeInLoginPageSuccessfully()"
+});
+formatter.result({
+  "duration": 1475067636,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 27895,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 5837156413,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "Verify user should navigate to Register Page",
+  "description": "",
+  "id": "login-and-register-page-navigate-functionality;verify-user-should-navigate-to-register-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "I am on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "I click on Register button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "I should be in Register page successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iAmOnHomePage()"
+});
+formatter.result({
+  "duration": 30766,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iClickOnRegisterButton()"
+});
+formatter.result({
+  "duration": 1265823657,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginAndRegisterFeature.iShouldBeInRegisterPageSuccessfully()"
+});
+formatter.result({
+  "duration": 458929029,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 27895,
+  "status": "passed"
+});
+});
